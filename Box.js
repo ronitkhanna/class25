6f8b80815b6c2082a40a41d3a188115320d4c4b9
ronitkhanna <81332598+ronitkhanna@users.noreply.github.com>
2021-04-24 18:54:50 -0500

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,10 @@
+class Box extends BaseClass{
+  constructor(x, y, widht, height) {
+    super(x,y,widht,height);
+    this.image=loadImage("sprites/wood1.png");
+  }
+ display(){
+   super.display();
+ }
+};
+
